f6a112a04b4384c1e4dafaef9170a973f55f3f72
diegojcastro <37005429+diegojcastro@users.noreply.github.com>
2020-08-21 09:53:28 -0400

    Title fragment, XML resource additions
    XML: added dimens for margin values, changed color scheme. Will change again.
    Title fragment: changed to work with data binding, adding title fragment to main. First compile+run worked for title properly.

---
+++ app/src/main/java/com/example/familybingo/screens/title/TitleFragment.kt
@@ -0,0 +1,26 @@
+package com.example.familybingo.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.familybingo.R
+import com.example.familybingo.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title, container, false)
+
+        return binding.root
+    }
+
+
+}
