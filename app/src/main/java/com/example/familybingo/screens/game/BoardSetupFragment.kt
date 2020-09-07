8bc0c2eeda50c1891e382d37adf26838ec2af6da
diegojcastro <37005429+diegojcastro@users.noreply.github.com>
2020-09-07 09:32:10 -0400

    Added edit bingoboard field functionality
    Added edit and done buttons, a semitransparent background to change focus to the edit view, a background for the edit view, proper code to open the keyboard automatically.

--- app/src/main/java/com/example/familybingo/screens/game/BoardSetupFragment.kt
+++ app/src/main/java/com/example/familybingo/screens/game/BoardSetupFragment.kt
@@ -1,12 +1,15 @@
 package com.example.familybingo.screens.game
 
-import androidx.lifecycle.ViewModelProviders
+
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.familybingo.R
@@ -24,7 +27,7 @@ class BoardSetupFragment : Fragment() {
         Log.i("BoardSetupFragment", "Called ViewModelProvider.get")
         viewModel = ViewModelProvider(this).get(BoardSetupViewModel::class.java)
 
-        binding.field11.setOnClickListener { editField(it) }
+        binding.field11.setOnClickListener { editField(it as TextView) }
 
         return binding.root
     }
@@ -38,16 +41,24 @@ class BoardSetupFragment : Fragment() {
     //Button click functions will go here, listeners on onCreateView I think
     //but they'll mostly just call viewModel.buttonFuctionName to do the work there
 
-    //Button to open field to input text should all go here though
 
-    private fun editField (view : View) {
+    // If I rotate the screen this is reset. Should I try changing it?
+    private fun editField (view : TextView) {
         val editText = binding.editTextBingoField
+        val backgroundBlur = binding.blurBackgroundImageView
+        val editButton = binding.editFieldButton
+        val doneButton = binding.doneEditingFieldButton
+        editButton.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        backgroundBlur.visibility = View.VISIBLE
         editText.visibility = View.VISIBLE
+        editText.setText(view.text)
         editText.requestFocus()
 
-        // TODO automatically open keyboard
-        // TODO grey out the background
-
+        //Gotta add getActivity() below because we're calling this from a fragment
+        // Show the keyboard.
+        val imm = getActivity()?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
 
     }
 
